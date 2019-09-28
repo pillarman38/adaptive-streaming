450c14fa7ffcdb38d3ee98cabb096988a8265d80
pillarman38 <connorwoodford@yahoo.com>
2019-09-27 22:43:45 -0700

    adds themoviedb documentation to server.js file

--- server.js
+++ server.js
@@ -31,3 +31,7 @@ var server = app.listen(port, function() {
 // to create ts files = ffmpeg -re -i "Fighting with My Family_t14.mkv" -codec copy -f segment -segment_list_flags +live -segment_time 10 blade%03d.ts
 
 // ffmpeg hls straming command = ffmpeg -i "Alita- Battle Angel-FPL_MainFeature_t99.mkv" -b:v 1M -g 60 -hls_list_size 0 output.m3u8
+
+//example of getting movie data as well as posters from themoviedb.com: https://api.themoviedb.org/3/search/movie?api_key=490cd30bbbd167dd3eb65511a8bf2328&query=Rogue%20One:%20A%20Star%20Wars%20Story
+
+// get movie posters like this: https://image.tmdb.org/t/p/w500/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg
